72e2577c64538c3c25f20a62002fd5bbab997bcc
caicry <caicry@gmail.com>
2012-04-23 22:30:46 +0800

    change database operate

--- gss_serial.py
+++ gss_serial.py
@@ -4,12 +4,17 @@ import gobject
 import os
 import sqlite3
 import sys
+from serial import Serial
 
 class QuickInputData:
     """ quick input data abstract class """
-    def __init__(self, caption, quicktext):
-        self.quicktext = caption
-        self.caption =  quicktext
+    def __init__(self, id, combox_id, combox_name, quick_caption, quick_text):
+        self.id = id
+        self.combox_id = combox_id
+        self.combox_name = combox_name
+        self.quick_caption = quick_caption
+        self.quick_text =  quick_text 
+    
 
 class ConfigureDlg(gtk.Dialog):
     """ Configure Dialog """
@@ -52,6 +57,7 @@ class BottomToolBar(gtk.Bin):
         self.feed_cb = feed_cb
         self.buttons = []
         self.buttons_handlers_map = {}
+        self.quickData = []
         self.hbox = hbox = gtk.HBox()
         
         # combox
@@ -68,48 +74,58 @@ class BottomToolBar(gtk.Bin):
     
     def LoadConfigData(self):
         self.comboxMap = {}  # comboxtext:(caption text, quicktext)
+        self.combox.clear()
         path = os.path.expanduser('~/.gss_serial')
         if os.path.exists(path) == False:
             os.mkdir(path)
         path = os.path.expanduser('~/.gss_serial/config.db')
         self.conn = conn = sqlite3.connect(path)
         c = conn.cursor()
-        c.execute('DROP TABLE IF EXISTS DATA')
-        c.execute('''CREATE TABLE IF NOT EXISTS DATA (Name TEXT, Caption TEXT, QuickText TEXT) ''')
-        c.execute('''CREATE TABLE IF NOT EXISTS CONFIG (COMBOX_POS INT) ''')
+        #c.execute('DROP TABLE IF EXISTS DATA')
+        c.execute('''CREATE TABLE IF NOT EXISTS DATA (id INTEGER PRIMARY KEY, pos INTEGER, Name TEXT, Caption TEXT, QuickText TEXT) ''')
+        c.execute('''CREATE TABLE IF NOT EXISTS CONFIG (COMBOX_POS INTEGER) ''')
         c.execute('SELECT Name FROM DATA')
         if len(c.fetchall()) == 0:
             c.execute('INSERT INTO CONFIG VALUES(0)')
-            c.execute(''' INSERT INTO DATA VALUES("defaults", "1", "2") ''')
-            c.execute(''' INSERT INTO DATA VALUES("defaults", "12", "2") ''')
-            c.execute(''' INSERT INTO DATA VALUES("defaults", "13", "2") ''')
-            c.execute(''' INSERT INTO DATA VALUES("defaultsX", "14", "2") ''')
-            c.execute(''' INSERT INTO DATA VALUES("defaultsX", "1", "2") ''')
-            c.execute(''' INSERT INTO DATA VALUES("defaultsX", "1", "2") ''')
+            c.execute(''' INSERT INTO DATA VALUES(NULL, 0, "defaults", "1", "21") ''')
+            c.execute(''' INSERT INTO DATA VALUES(NULL, 0, "defaults", "12", "22") ''')
+            c.execute(''' INSERT INTO DATA VALUES(NULL, 0, "defaults", "13", "23") ''')
+            c.execute(''' INSERT INTO DATA VALUES(NULL, 1, "defaultsX", "14", "24") ''')
+            c.execute(''' INSERT INTO DATA VALUES(NULL, 1, "defaultsX", "1", "25") ''')
+            c.execute(''' INSERT INTO DATA VALUES(NULL, 1, "defaultsX", "1", "26") ''')
             
         # GET combox pos
         c.execute('SELECT COMBOX_POS FROM CONFIG')
         self.combox_cur = c.fetchone()[0]
-        
+
         # store tmp data into self.comboxMap
-        c.execute('SELECT DISTINCT Name FROM DATA')
+        del self.quickData[:]
+        c.execute('SELECT * FROM DATA')
         for i in c.fetchall():
-            # combox_text
-            for j in i:
-                # btn_info
-                str = 'SELECT Caption,QuickText FROM DATA WHERE Name="%s"' % j
-                c.execute(str)
-                btn_info = [] 
-                self.combox.append_text(j)
-                for k in c.fetchall():
-                    if len(k[0])>0 and len(k[1])>0:
-                        btn_info.append(k)
-                self.comboxMap[j] = btn_info               
+            data = QuickInputData(i[0], i[1], i[2], i[3], i[4])
+            self.quickData.append(data)
+        
+        for i in self.GetQuickComboxNames():
+            self.combox.append_text(i)
         c.close()              
         
+    def GetQuickComboxNames(self):
+        data = []
+        j = -1
+        for item in self.quickData:
+            if j != item.combox_id:
+                data.append(item.combox_name)
+                j = item.combox_id
+        return data
+    
+    def GetQuickButtonsByComboxPos(self, pos):
+        data = []
+        for item in self.quickData:
+            if item.combox_id == pos:
+                data.append(item)
+        return data
+        
     def RemoveAllBtns(self):
-        print "RemoveAll"
-        print "!!buttons len:%d" % len(self.buttons)
         for i in self.buttons:
             self.hbox.remove(i)
             for handler in self.buttons_handlers_map[i]:
@@ -117,21 +133,21 @@ class BottomToolBar(gtk.Bin):
             self.buttons.remove(i)                
             i.destroy()
         
+        # why can't clear list????
         for i in self.buttons:
             self.hbox.remove(i)
             for handler in self.buttons_handlers_map[i]:
                 i.disconnect(handler)
             self.buttons.remove(i)                
             i.destroy()
-        print "buttons len:%d" % len(self.buttons)
+        #print "buttons len:%d" % len(self.buttons)
         self.buttons_handlers_map.clear()
    
-    def UpdateBtns(self, value): 
-        #print value
+    def UpdateBtns(self, data): 
         self.quickTextMap = {}
-        for i in value:
-            btn = gtk.Button(i[0])
-            self.quickTextMap[btn] = i[1]
+        for item in data:
+            btn = gtk.Button(item.quick_caption)
+            self.quickTextMap[btn] = item
             self.hbox.pack_start(btn, False, False, 0)
             self.buttons.append(btn)
             handler1 = btn.connect("button-press-event", self.OnBtnRightClick)
@@ -140,16 +156,9 @@ class BottomToolBar(gtk.Bin):
         self.hbox.show_all()
         
     def SwitchComboxPos(self, pos):
-        tick=0
-        for key in self.comboxMap:
-            if tick == pos:
-                self.RemoveAllBtns()
-                self.UpdateBtns(self.comboxMap[key])
-                break
-            tick+=1
-        
-    def OnComboxChanged(self, widget):            
-        self.SwitchComboxPos(self.combox.get_active())
+        self.combox.set_active(pos)
+        self.RemoveAllBtns()
+        self.UpdateBtns(self.GetQuickButtonsByComboxPos(pos))
         
     # --- start combox btn event
     def OnComboxRightClick(self, widget):
@@ -162,13 +171,15 @@ class BottomToolBar(gtk.Bin):
             self.buttons.append(button)
         dlg.destroy()
         
+    def OnComboxChanged(self, widget):            
+        self.SwitchComboxPos(self.combox.get_active())
+        
     # --- end combox btn event
     
     # ---start quick btn event
     def OnBtnClick(self, widget):
-        print 'OnBtnClick....'
         if self.feed_cb:
-            self.feed_cb(self.quickTextMap[widget])    
+            self.feed_cb(self.quickTextMap[widget].quick_text + '\n')    
         
     def OnBtnRightClick(self, widget, event):
         if event.button == 3:
@@ -188,12 +199,29 @@ class BottomToolBar(gtk.Bin):
             
     def OnMenuModifyBtn(self, event, widget):
         # TODO:
+        dlg = ConfigureDlg()
+        item = self.quickTextMap[widget]
+        dlg.entry_caption.set_text(item.quick_caption)
+        dlg.entry_quickInput.set_text(item.quick_text)
+        if dlg.run() == gtk.RESPONSE_OK :
+            # first update database
+            if not (dlg.entry_caption.get_text_length() == 0 or dlg.entry_quickInput.get_text_length() ==0) :
+                item.quick_caption = dlg.entry_caption.get_text()
+                item.quick_text = dlg.entry_quickInput.get_text()
+                c = self.conn.cursor()
+                str = 'UPDATE DATA SET Caption="%s", QuickText="%s" WHERE id=%d' % (item.quick_caption, item.quick_text, item.id)
+                print str
+                c.execute(str)
+                self.conn.commit() 
+                c.close()
+                self.LoadConfigData()
+        dlg.destroy()
         pass
         
     def OnMenuDelBtn(self, event, widget):
-        # TODO:
+        # TODO: remove widget and update ui
         pass
-    # ---end quick btn event
+        # ---end quick btn event
         
     def do_size_request(self, req):
         (w,h) = self.hbox.size_request()
@@ -216,14 +244,33 @@ class MyDlg(gtk.Window):
         # vbox 
         self.vbox = vbox = gtk.VBox() 
         # vte ctrl
+        self.serial = Serial(port='/dev/ttyUSB0', baudrate=115200)
+        self.serial.setTimeout(0)
+        gobject.io_add_watch(self.serial, gobject.IO_IN, self.OnSerialRead)
         self.terminal = vte.Terminal() 
+        self.terminal.connect("commit", self.OnTerminalWrite)
         vbox.add(self.terminal)
+        
         # botton toolbar
-        self.toolbar = toolbar = BottomToolBar()
+        self.toolbar = toolbar = BottomToolBar(feed_cb = self.feedTerminal)
         vbox.pack_end(toolbar)
-       
+    
         self.add(vbox) 
         self.show_all()
+
+    def OnTerminalWrite(self, widget, data, size):
+        if self.serial.isOpen():
+            self.serial.write(data)
+    
+    def OnSerialRead(self, widget, condition):
+        data = self.serial.read(1024)
+        self.terminal.feed(data)
+        return True
+    
+    def feedTerminal(self, data):
+        if self.serial.isOpen():
+            self.serial.write(data)
+        
         
 if __name__ == "__main__":
     MyDlg()
